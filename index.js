66a26837cdea5f0a55802f9487bfa4813ce3e64c
haseharshit <haseharshit@gmail.com>
2024-03-27 17:35:08 +0530

    eslint Script added

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-
-const express = require("express")
-const morgan = require("morgan")
-const cors =require("cors")
+const express = require('express')
+const morgan = require('morgan')
+const cors =require('cors')
 const app= express()
 const Contact = require('./mongo')
 app.use(cors())
 app.use(express.static('dist'))
-morgan.token('body', (req)=>{
+morgan.token('body', (req) => {
     return `${JSON.stringify(req.body)}`
 })
 
 app.use(express.json())
 
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
     if(req.method==='POST' && res.status>300){
-        morgan(":method :url HTTP/:http-version :status :res[content-length] :response-time ms :body ")(req, res, next)
+        morgan(':method :url HTTP/:http-version :status :res[content-length] :response-time ms :body ')(req, res, next)
     }
 
-    next();
+    next()
 })
 
 
-app.get('/api/persons', (req, res, next)=>{
+app.get('/api/persons', (req, res, next) => {
     Contact.find({}).then(contacts => {
         console.log(contacts)
         res.send(contacts)
@@ -36,14 +35,14 @@ app.get('/info',async (req, res, next) => {
         <p>${Date()}</p>
         `)
     })
-    .catch(err => next(err))
+        .catch(err => next(err))
 
 
 })
 
-app.get('/api/persons/:id', (req, res, next)=>{
+app.get('/api/persons/:id', (req, res, next) => {
     const id= (req.params.id)
-    Contact.findById(id).then(contact=> {
+    Contact.findById(id).then(contact => {
         contact?res.send(contact): res.status(404).send({})
     }).catch(err => next(err))
 })
@@ -54,20 +53,20 @@ app.delete('/api/persons/:id',(req,res, next) => {
     const id= (req.params.id)
     console.log(id)
     Contact.findByIdAndDelete(id).then(result => {
-        result?res.send(result):res.status(400).send({'error': "No record with provided id"})
+        result?res.send(result):res.status(400).send({ 'error': 'No record with provided id' })
         return
     })
-    .catch(err=>{
-        next(err)
+        .catch(err => {
+            next(err)
         // res.status(502).send({"error": `${err}`})
-    })
+        })
 
 
 })
 
 const handleInsert = async (req, res, next) => {
 
-    const {name, number} = req.body
+    const { name, number } = req.body
     // if(!name){
     //     next("Name attribute not found")
     //     // res.status(400).send({"error": "Name field is required"})
@@ -79,14 +78,14 @@ const handleInsert = async (req, res, next) => {
     //     next({'error':'Name Shuld be unique'})
     //     return
     // }
-    const newContact= new Contact({
-        name: name,
-        number: number
-    })
-    const result= await newContact.save()
-    console.log(result)
-    res.status(201).send(result)
-    return
+        const newContact= new Contact({
+            name: name,
+            number: number
+        })
+        const result= await newContact.save()
+        console.log(result)
+        res.status(201).send(result)
+        return
     }
     catch(err){
         next(err)
@@ -95,23 +94,22 @@ const handleInsert = async (req, res, next) => {
 
 
 
-    }
+}
 
 app.post('/api/persons', handleInsert
 )
 
-app.put('/api/persons/:id', (req, res) => {
-    const id= req.params.id
-    Contact.findOneAndUpdate({name: req.body.name}, {number: req.body.number}, {new: true, runValidators:true, context:'query'}).then(result => {
+app.put('/api/persons/:id', (req, res, next) => {
+    Contact.findOneAndUpdate({ name: req.body.name }, { number: req.body.number }, { new: true, runValidators:true, context:'query' }).then(result => {
         console.log(result)
         res.send(result)
     })
-    .catch(err => next(err))
+        .catch(err => next(err))
 })
 
 
 const unknownPoint =(req, res) => {
-    res.status(404).send({'error': "Unkown endpoint"})
+    res.status(404).send({ 'error': 'Unkown endpoint' })
 }
 
 app.use(unknownPoint)
@@ -119,15 +117,15 @@ app.use(unknownPoint)
 const errorHandler = (error, req, res, next) => {
     console.log(error.name)
     if( ['ValidationError','CastError', 'MongooseError', 'MongoServerError' ].includes(error.name)){
-        res.status(400).send({'error': `${error.message}`})
+        res.status(400).send({ 'error': `${error.message}` })
         return
     }
     res.status(400).send(error)
-
+    next()
 }
 
 app.use(errorHandler)
 
 
-const PORT= process.env.PORT || 3000 
-app.listen(PORT, ()=> console.log(`Server port running on ${PORT}`))
+const PORT= process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server port running on ${PORT}`))
--- mongo.js
+++ mongo.js
@@ -1,14 +1,14 @@
-const mongoose= require("mongoose")
+const mongoose= require('mongoose')
 require('dotenv').config()
 
 mongoose.set('strictQuery', false)
 async function connectToDB() {
     try{
         await mongoose.connect(process.env.MONGOD_URI)
-        console.log("Connected to Cloud Atlas DB");
+        console.log('Connected to Cloud Atlas DB')
     }
     catch(err) {
-        console.log(`Failed to connect`)
+        console.log('Failed to connect')
     }}
 connectToDB()
 const contactSchema = mongoose.Schema({
@@ -24,12 +24,12 @@ const contactSchema = mongoose.Schema({
             validator: (v) => {
                 return /^\d{3}-\d{4}$/.test(v)
             },
-            message: "Number's format is invalid"
+            message: 'Number\'s format is invalid'
         }
     }
 })
 contactSchema.set('toJSON', {
-    transform : (document, returnObject)=>{
+    transform : (document, returnObject) => {
         returnObject.id= returnObject._id
         delete returnObject._id
         delete returnObject.__v
