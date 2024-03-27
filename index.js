01562a4c72ffd333e205caacf08201ee9d7ef7e9
haseharshit <haseharshit@gmail.com>
2024-03-27 16:49:44 +0530

    Deploy frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const morgan = require("morgan")
 const cors =require("cors")
 const app= express()
+const Contact = require('./mongo')
 app.use(cors())
 app.use(express.static('dist'))
 morgan.token('body', (req)=>{
@@ -12,81 +13,121 @@ morgan.token('body', (req)=>{
 app.use(express.json())
 
 app.use((req, res, next)=> {
-    if(req.method==='POST'){
+    if(req.method==='POST' && res.status>300){
         morgan(":method :url HTTP/:http-version :status :res[content-length] :response-time ms :body ")(req, res, next)
     }
+
     next();
 })
 
-let contacts=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    {
-        "id":642,
-        "name": "Torshabla",
-        "number": "642642642"
+
+app.get('/api/persons', (req, res, next)=>{
+    Contact.find({}).then(contacts => {
+        console.log(contacts)
+        res.send(contacts)
+    }).catch(err => {
+        next(err)
+    })
+})
+app.get('/info',async (req, res, next) => {
+    Contact.find({}).then(result => {
+        res.send(`
+        <p>This phonebook has info for ${result.length} people</p>
+        <p>${Date()}</p>
+        `)
+    })
+    .catch(err => next(err))
+
+
+})
+
+app.get('/api/persons/:id', (req, res, next)=>{
+    const id= (req.params.id)
+    Contact.findById(id).then(contact=> {
+        contact?res.send(contact): res.status(404).send({})
+    }).catch(err => next(err))
+})
+
+
+
+app.delete('/api/persons/:id',(req,res, next) => {
+    const id= (req.params.id)
+    console.log(id)
+    Contact.findByIdAndDelete(id).then(result => {
+        result?res.send(result):res.status(400).send({'error': "No record with provided id"})
+        return
+    })
+    .catch(err=>{
+        next(err)
+        // res.status(502).send({"error": `${err}`})
+    })
+
+
+})
+
+const handleInsert = async (req, res, next) => {
+
+    const {name, number} = req.body
+    // if(!name){
+    //     next("Name attribute not found")
+    //     // res.status(400).send({"error": "Name field is required"})
+    //     return
+    // }
+    try{
+    // const existingUser = await Contact.find({name: name})
+    // if(existingUser.length){
+    //     next({'error':'Name Shuld be unique'})
+    //     return
+    // }
+    const newContact= new Contact({
+        name: name,
+        number: number
+    })
+    const result= await newContact.save()
+    console.log(result)
+    res.status(201).send(result)
+    return
     }
-]
-
-app.get('/api/persons', (req, res)=>{
-    res.send(contacts)
-})
-app.get('/info', (req, res) => {
-    res.send(`
-    <p>This phonebook has info for ${contacts.length} people</p>
-    <p>${Date()}</p>
-    `)
-})
-
-app.get('/api/persons/:id', (req, res)=>{
-    const id= Number(req.params.id)
-    const body=contacts.find((contact)=> contact.id===id)
-    body?res.send(body): res.status(404).send({})
-})
-
-app.delete('/api/persons/:id', (req,res) => {
-    const id= Number(req.params.id)
-    const body=contacts.find((contact)=> contact.id===id)
-    if(body){
-        contacts=contacts.filter(contact => contact.id!==body.id)
-        return res.send(body)
+    catch(err){
+        next(err)
+        // res.status(502).send({"error": `${err}`})
     }
-    return res.status(404).send({"error":"Person with provided id is not found"})
 
-})
 
-app.post('/api/persons', (req, res)=>{
-    if(req.body.name){
-        if(contacts.find(contact=> contact.name.toLowerCase()===req.body.name.toLowerCase()))
-            return res.status(400).send({error:"Name must be unique"})
-        const newContact = {
-            id: Math.round(Math.random()*1000000),
-            name: req.body.name,
-            number: req.body.number?req.body.number:""}
-        
-        contacts=contacts.concat(newContact)
-        return res.json(newContact)
+
     }
-    res.status(400).send({error: "Unable to get name attribute"})
+
+app.post('/api/persons', handleInsert
+)
+
+app.put('/api/persons/:id', (req, res) => {
+    const id= req.params.id
+    Contact.findOneAndUpdate({name: req.body.name}, {number: req.body.number}, {new: true, runValidators:true, context:'query'}).then(result => {
+        console.log(result)
+        res.send(result)
+    })
+    .catch(err => next(err))
 })
+
+
+const unknownPoint =(req, res) => {
+    res.status(404).send({'error': "Unkown endpoint"})
+}
+
+app.use(unknownPoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.log(error.name)
+    if( ['ValidationError','CastError', 'MongooseError', 'MongoServerError' ].includes(error.name)){
+        res.status(400).send({'error': `${error.message}`})
+        return
+    }
+    res.status(400).send(error)
+
+}
+
+app.use(errorHandler)
+
 
 const PORT= process.env.PORT || 3000 
 app.listen(PORT, ()=> console.log(`Server port running on ${PORT}`))
---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose= require("mongoose")
+require('dotenv').config()
+
+mongoose.set('strictQuery', false)
+async function connectToDB() {
+    try{
+        await mongoose.connect(process.env.MONGOD_URI)
+        console.log("Connected to Cloud Atlas DB");
+    }
+    catch(err) {
+        console.log(`Failed to connect`)
+    }}
+connectToDB()
+const contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        unique: true
+    },
+    number: {
+        type:String,
+        validate : {
+            validator: (v) => {
+                return /^\d{3}-\d{4}$/.test(v)
+            },
+            message: "Number's format is invalid"
+        }
+    }
+})
+contactSchema.set('toJSON', {
+    transform : (document, returnObject)=>{
+        returnObject.id= returnObject._id
+        delete returnObject._id
+        delete returnObject.__v
+    }
+})
+
+module.exports= mongoose.model('Contact', contactSchema)
